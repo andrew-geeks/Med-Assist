09ef3082fb49570ca42f240e5ee981804b6611c1
Andrew George Issac <andrewgeorgeissac@gmail.com>
2024-12-09 00:05:51 +0530

    main

---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./navbar";
+
+function Home(){
+
+    return(
+        <div>
+            <Navbar/>
+            <h1>This is Home</h1>
+        </div>
+        
+    )
+}
+
+export default Home;
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import '../styles/navbar.css';
+
+function Navbar(){
+    return(
+        <div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Login</li>
+                    <li>Signup</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+
+export default Navbar;
